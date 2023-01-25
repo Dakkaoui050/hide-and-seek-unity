8c917cf2e2424df2d5f6cc0f7b1f85acccd6efbc
Dakkaoui050 <80848743+Dakkaoui050@users.noreply.github.com>
2023-01-25 21:50:04 +0100

    audio done

--- hide and seek/Assets/Script/UI/option.cs
+++ hide and seek/Assets/Script/UI/option.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class option : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioSource audioSource;
+    public Slider volumeSlider;
+
     void Start()
     {
-        
+        DontDestroyOnLoad(audioSource);
+        // Set the initial value of the volume slider to match the current volume of the audio source
+        volumeSlider.value = audioSource.volume;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolume()
     {
-        
+        // Set the volume of the audio source to the value of the volume slider
+        audioSource.volume = volumeSlider.value;
     }
 }
