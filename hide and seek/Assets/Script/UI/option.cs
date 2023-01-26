171cf129c9729cd50c0c33f177655e4e8f48d940
Dakkaoui050 <80848743+Dakkaoui050@users.noreply.github.com>
2023-01-26 13:07:09 +0100

    bvnm

--- hide and seek/Assets/Script/UI/option.cs
+++ hide and seek/Assets/Script/UI/option.cs
@@ -2,17 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class option : MonoBehaviour
 {
+    //music setting
     public AudioSource audioSource;
     public Slider volumeSlider;
 
+    //
+    public Toggle fullscreenTog, vsyncTog;
+
+    public List<ResItem> resolutions = new List<ResItem>();
+    private int selectedResolution;
+
+    public TMP_Text resolutionLabel;
     void Start()
     {
         DontDestroyOnLoad(audioSource);
         // Set the initial value of the volume slider to match the current volume of the audio source
         volumeSlider.value = audioSource.volume;
+
+        // size de screen
+        fullscreenTog.isOn = Screen.fullScreen;
+
+        //vSync setting
+        if (QualitySettings.vSyncCount == 0)
+        {
+            vsyncTog.isOn = false;
+        }
+        else
+        {
+            vsyncTog.isOn = true;
+        }
+
+        bool foundRes = false;
+        for (int i = 0; i < resolutions.Count; i++)
+        {
+            if (Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundRes = true;
+
+                selectedResolution = i;
+
+                UpdateResLabel();
+            }
+        }
+
+        if (!foundRes)
+        {
+            ResItem newRes = new ResItem();
+            newRes.horizontal = Screen.width;
+            newRes.vertical = Screen.height;
+
+            resolutions.Add(newRes);
+            selectedResolution = resolutions.Count - 1;
+
+            UpdateResLabel();
+        }
+
     }
 
     public void SetVolume()
@@ -20,4 +68,55 @@ public class option : MonoBehaviour
         // Set the volume of the audio source to the value of the volume slider
         audioSource.volume = volumeSlider.value;
     }
+
+
+   
+
+    public void resolutionsLeft()
+    {
+        selectedResolution--;
+        if (selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void resolutionsRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
+    public void ApplyGraphics()
+    {
+        if (vsyncTog.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenTog.isOn);
+    }
+
+
+}
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
